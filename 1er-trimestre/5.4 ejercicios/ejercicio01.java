c02f5169c1d6fdb926c520ba0370c1faccf18bcf
gonzalosalmeron <gonzxlosalmeron@gmailcom>
2020-11-03 10:14:17 +0100

    subida ejercicios 5.4

---
+++ 1er-trimestre/5.4 ejercicios/ejercicio01.java
@@ -0,0 +1,17 @@
+/**
+* 
+*Mostrar multiplos de 5 utilizando bucle for
+* 
+*
+* @author Gonzalo Salmerón Robles
+*/
+public class ejercicio01{
+  public static void main(String[] args){
+    System.out.println("Los multiplos de 5 son: ");
+    System.out.println("-------------------------------------");
+    for(int i = 0; i <= 20; i++){
+      System.out.println(i * 5);
+    }
+    System.out.println("-------------------------------------");
+  }
+}
---
+++ 1er-trimestre/5.4 ejercicios/ejercicio02.java
@@ -0,0 +1,9 @@
+/**
+* 
+*Mostrar multiplos de 5 utilizando bucle for
+* 
+*
+* @author Gonzalo Salmerón Robles
+*/
+public class ejercicio01{
+  public static void main(String[] args){
