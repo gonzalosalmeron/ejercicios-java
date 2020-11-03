41df1de5babb96c4b87ac5e8c021a553de922f07
gonzalosalmeron <gonzxlosalmeron@gmailcom>
2020-11-03 15:52:25 +0100

    Subida ejercicios 5.4

---
+++ 1er-trimestre/5.4 ejercicios/ejercicio04.java
@@ -0,0 +1,16 @@
+/**
+* 
+*Mostrar números de 320 al 160 de 20 en 20
+* 
+*
+* @author Gonzalo Salmerón Robles
+*/
+public class ejercicio04{
+  public static void main(String[] args){
+    System.out.println("----------------------------------------------");
+    for(int i = 320; i >=160; i = i - 20){
+      System.out.println(i);
+    }
+    System.out.println("----------------------------------------------");
+  }
+}
